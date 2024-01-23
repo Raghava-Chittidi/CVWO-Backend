c18561ed8313d7e073f2a9787ed6a88bfcd6ad52
Raghava <chinnu4148@gmail.com>
2024-01-24 00:23:06 +0800

    Env file bug

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,10 +11,10 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() (*gorm.DB, error) {
-	err := godotenv.Load("../../.env")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// err := godotenv.Load("../../.env")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	
 	dsn := os.Getenv("DB_URL")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
